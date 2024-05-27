592355dff81beb7fe5ea052eb4253f5e232993d2
Pau Guerrero Prado <pauguerrero@gmail.com>
2024-05-27 17:39:55 +0200

    add d3 lib

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function sumar(num1, num2) {
+  console.log(num1 + num2);
+}
+
+sumar(2, 3);
+
+const restar = (num1, num2) => {
+  console.log(num1 - num2);
+};
+
+restar(2, 3);
+
+document.querySelector('div').innerHTML = "<p>vengo de JS</p>";
