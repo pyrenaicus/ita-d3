36cd88ac78658adae8ca5f705718661899b77127
Pau Guerrero Prado <pauguerrero@gmail.com>
2024-05-28 19:24:58 +0200

    horitz bar

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ sep = 5
 marge = 20
 multAlt = 4
 IdDiv = '#chart-2'
-ampleGrafic = dades.length * (ample + sep) + marge * 2
-altGrafic = multAlt * Math.max(...dades) + marge * 2
-graficBarres(dades, colors, ample, sep, marge, multAlt, IdDiv)
+altGrafic = dades.length * (ample + sep) + marge * 2
+ampleGrafic = multAlt * Math.max(...dades) + marge * 2
+graficBarresHoritz(dades, colors, ample, sep, marge, multAlt, IdDiv)
 
 function graficBarres(dades, colors, ample, sep, marge, multAlt, idDiv) {
   // creem el conteniodr gràfic
@@ -57,6 +57,39 @@ function graficBarres(dades, colors, ample, sep, marge, multAlt, idDiv) {
     .attr('x', (d, i) => marge + i * (ample + sep) + ample / 2)
     .attr('y', (d, i) => marge + multAlt * Math.max(...dades) - d * multAlt - sepText)
     .attr('text-anchor', 'middle')
+    .text((d) => d)
+
+}
+// horizontal bar
+function graficBarresHoritz(dades, colors, ample, sep, marge, multAlt, idDiv) {
+  // creem el conteniodr gràfic
+  let svg = d3.select(idDiv)
+    .append('svg')
+    .attr('width', ampleGrafic)
+    .attr('height', altGrafic)
+
+  // seleccionem tots els rectangles que crearem
+  let rectangles = svg.selectAll('rect')
+
+  rectangles
+    .data(dades)
+    .enter()
+    .append('rect')
+    .attr('height', 50)
+    .attr('width', (d, i) => d * multAlt)
+    .attr('x', (d) => marge)
+    .attr('y', (d, i) => marge + i * (ample + sep))
+    // .attr('fill', 'plum')
+    .attr('fill', (_, i) => colors[i])
+
+  let text = svg.selectAll('text')
+
+  text
+    .data(dades)
+    .enter()
+    .append('text')
+    .attr('x', (d, i) => marge + multAlt * Math.max(...dades) - Math.max(...dades) - d)
+    .attr('y', (d, i) => marge + i * (ample + sep) + ample / 2)
     .text((d) => d)
 
 }
